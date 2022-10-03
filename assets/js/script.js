8f524034186e9c8ba30b2514f4eacac5511cbba9
Louise Stanley <somekindofdesign@gmail.com>
2022-10-03 09:20:48 +0000

    Add code to calcualte two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
-function runGame() {
+document.addEventListener("DOMContentLoaded", function() {
+  let buttons = this.getElementsByTagName("button");
 
+  for (let button of buttons) {
+    button.addEventListener("click", function() {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked submit!");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        alert(`You clicked $(gameType)`);
+      }
+    })
+  }
+})
+
+function runGame() {
+  let num1 = math.floor(math.random() * 25) + 1;
+  let num2 = math.floor(math.random() * 25) + 1;
 }
 
 function getAnswer() {
@@ -27,5 +43,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultipleQuestion() {
-    
+
 }
