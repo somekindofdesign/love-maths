590b983670fdbd64af0502406f3951dc481f3879
Louise Stanley <somekindofdesign@gmail.com>
2022-10-03 08:37:02 +0000

    Setup structure of game function

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function getAnswer() {
+
+}
+
+function calculateAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionalQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultipleQuestion() {
+    
+}
